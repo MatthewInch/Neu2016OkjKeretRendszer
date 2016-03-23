40edeef191180b45d12657253281d671ff6cfe3d
~execthts <me@executioner1337.net>
2016-03-23 08:34:59 +0100

    form bgcolor -> Midnightblue

--- WCFServiceLibrary/MyMessage/Form1.Designer.cs
+++ WCFServiceLibrary/MyMessage/Form1.Designer.cs
@@ -28,85 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtAllMessage = new System.Windows.Forms.TextBox();
-            this.txtMessage = new System.Windows.Forms.TextBox();
-            this.btnSend = new System.Windows.Forms.Button();
-            this.txtReceived = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // txtAllMessage
-            // 
-            this.txtAllMessage.Location = new System.Drawing.Point(12, 136);
-            this.txtAllMessage.Multiline = true;
-            this.txtAllMessage.Name = "txtAllMessage";
-            this.txtAllMessage.ReadOnly = true;
-            this.txtAllMessage.Size = new System.Drawing.Size(363, 346);
-            this.txtAllMessage.TabIndex = 0;
-            // 
-            // txtMessage
-            // 
-            this.txtMessage.Location = new System.Drawing.Point(12, 85);
-            this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(282, 20);
-            this.txtMessage.TabIndex = 1;
-            // 
-            // btnSend
-            // 
-            this.btnSend.Location = new System.Drawing.Point(300, 85);
-            this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
-            this.btnSend.TabIndex = 2;
-            this.btnSend.Text = "Send";
-            this.btnSend.UseVisualStyleBackColor = true;
-            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
-            // 
-            // txtReceived
-            // 
-            this.txtReceived.Location = new System.Drawing.Point(440, 25);
-            this.txtReceived.Multiline = true;
-            this.txtReceived.Name = "txtReceived";
-            this.txtReceived.ReadOnly = true;
-            this.txtReceived.Size = new System.Drawing.Size(335, 457);
-            this.txtReceived.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 117);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Válasz";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(437, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Érkezett";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 494);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtReceived);
-            this.Controls.Add(this.btnSend);
-            this.Controls.Add(this.txtMessage);
-            this.Controls.Add(this.txtAllMessage);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.txtAllMessage = new System.Windows.Forms.TextBox();
+			this.txtMessage = new System.Windows.Forms.TextBox();
+			this.btnSend = new System.Windows.Forms.Button();
+			this.txtReceived = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// txtAllMessage
+			// 
+			this.txtAllMessage.Location = new System.Drawing.Point(12, 136);
+			this.txtAllMessage.Multiline = true;
+			this.txtAllMessage.Name = "txtAllMessage";
+			this.txtAllMessage.ReadOnly = true;
+			this.txtAllMessage.Size = new System.Drawing.Size(363, 346);
+			this.txtAllMessage.TabIndex = 0;
+			// 
+			// txtMessage
+			// 
+			this.txtMessage.Location = new System.Drawing.Point(12, 85);
+			this.txtMessage.Name = "txtMessage";
+			this.txtMessage.Size = new System.Drawing.Size(282, 20);
+			this.txtMessage.TabIndex = 1;
+			// 
+			// btnSend
+			// 
+			this.btnSend.Location = new System.Drawing.Point(300, 85);
+			this.btnSend.Name = "btnSend";
+			this.btnSend.Size = new System.Drawing.Size(75, 23);
+			this.btnSend.TabIndex = 2;
+			this.btnSend.Text = "Send";
+			this.btnSend.UseVisualStyleBackColor = true;
+			this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+			// 
+			// txtReceived
+			// 
+			this.txtReceived.Location = new System.Drawing.Point(440, 25);
+			this.txtReceived.Multiline = true;
+			this.txtReceived.Name = "txtReceived";
+			this.txtReceived.ReadOnly = true;
+			this.txtReceived.Size = new System.Drawing.Size(335, 457);
+			this.txtReceived.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 117);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Válasz";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(437, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(46, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Érkezett";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.MidnightBlue;
+			this.ClientSize = new System.Drawing.Size(787, 494);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtReceived);
+			this.Controls.Add(this.btnSend);
+			this.Controls.Add(this.txtMessage);
+			this.Controls.Add(this.txtAllMessage);
+			this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(10)))), ((int)(((byte)(170)))), ((int)(((byte)(255)))));
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
