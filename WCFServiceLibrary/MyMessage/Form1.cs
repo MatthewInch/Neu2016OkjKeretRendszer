b4693726a2520be708aff1d3617a98276a36e3ad
kepesspo <szecsimark@gmail.com>
2016-03-30 10:57:09 +0200

    finish

--- WCFServiceLibrary/MyMessage/Form1.cs
+++ WCFServiceLibrary/MyMessage/Form1.cs
@@ -16,6 +16,7 @@ namespace MyMessage
     {
         private ServiceHost _host;
         private ServiceHost _innerHost;
+        private MessageService.MessageServiceClient _client;
 
         public Form1()
         {
@@ -24,12 +25,21 @@ namespace MyMessage
 
         private void btnSend_Click(object sender, EventArgs e)
         {
-            if (txtMessage.Text.Trim() != string.Empty)
+            try
             {
-                MessageService.MessageServiceClient client = new MessageService.MessageServiceClient();
-                var output = client.GetMessage(txtMessage.Text);
-                txtAllMessage.Text += output + "\r\n";
+                if (txtMessage.Text.Trim() != string.Empty)
+                {
+                    //MessageService.MessageServiceClient client = new MessageService.MessageServiceClient();
+                    var output = _client.GetMessage(txtMessage.Text);
+                    txtAllMessage.Text += output + "\r\n";
+                }
             }
+            catch (Exception exp)
+            {
+
+                MessageBox.Show(string.Format("Error: {0}", exp.Message));
+            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -80,5 +90,22 @@ namespace MyMessage
         {
 
         }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if(txtServerIP.Text != string.Empty)
+            {
+                if(_client != null)
+                {
+                    _client.Close();
+                    _client = null;
+                }
+
+                _client = new MessageService.MessageServiceClient();
+                _client.Endpoint.Address = new EndpointAddress(string.Format("http://{0}:8080/WCFServiceLibrary/MessageService/", txtServerIP.Text));
+                _client.Open();
+            }
+        }
+        
     }
 }
